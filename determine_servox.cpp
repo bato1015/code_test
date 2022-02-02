0bd773be8f864ef07ad7e3a407a9be246c8e7c07
Bato Riko <s20c1101jd@s.chib>
2022-02-02 17:09:28 +0900

    testtestste

--- determine_servox.cpp
+++ determine_servox.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <fstream>
 
-#define ACCELITE_SERV_NUM 5
+#define ACCELITE_SERV_NUM 1
 
 void servoErrorstandardCheckCommand();
 void servoErrorDetailCheckCommand(int servoID, unsigned char readbuf, int re);
@@ -29,30 +29,37 @@ void servoErrorstandardCheckCommand(int servoID, unsigned char readbuf)
     std::string filename = "sample.txt";
     write.open(filename, std::ios::app);
 
-    std::stringstream ss;
     std::vector<std::string> error_list = {"system_error", "motor_error", "UART_error", "command_error"};
-    unsigned char sendcommand[] = {0x01, 0x02, 0x03, 0x04, 0x05};
+    unsigned char sendcommand[] = {0x01, 0x02, 0x03, 0x04};
     //struct ServoStatus s;
     //std::cout << to_binString(readbuf) << "\n";
     if (readbuf != 0x99)
     {
         for (int i = 0; i < error_list.size(); i++)
         {
+            std::stringstream ss;
             unsigned char check_bit = 1;
             if (readbuf & (check_bit << i))
             {
                 // std::cout << "test" << std::endl;
                 ss << "mode:" << i << " "
-                   << "[" << servoID << "]" << error_list[i] << std::endl;
-                sendbuf = 0x04; //送って受け取る
+                   << "[" << servoID << "]" << error_list[i];
                 sendbuf = sendcommand[i];
+                sendbuf = 0x05; //送って受け取る
                 //std::cout << to_binString(sendbuf) << "\n";
 
+                if (sendbuf == 0x00)
+                {
+                    ss << "\n"
+                       << " Not detail error" << std::endl;
+                }
+
                 std::string s = ss.str();
+                std::cout << s << std::endl;
                 const char *msg = s.c_str();
                 if (s.size() != 0)
                 {
-                    std::cout << msg;
+                    //std::cout << msg;
                     write << msg << std::endl;
                     //syslog(LOG_DEBUG, "test");
                 }
@@ -84,8 +91,7 @@ void servoErrorDetailCheckCommand(int servoID, unsigned char readbuf, int re) //
 
             if (readbuf & (check_bit << i))
             {
-                ss << " "
-                   << "mode:" << i << " "
+                ss << " mode:" << i << " "
                    << "[" << servoID << "]" << error_detail_list[re][i] << std::endl;
             }
         }
@@ -106,11 +112,12 @@ void allErrorJuging()
     {
         unsigned char readbuf;
         //struct ServoStatus s;
-        readbuf = 0x08; //送って受け取る
-        std::cout << to_binString(readbuf) << "\n";
+        readbuf = 0x7; //送って受け取る
+        //std::cout << to_binString(readbuf) << "\n";
         servoErrorstandardCheckCommand(i, readbuf);
     }
 }
+
 // 数値を２進数文字列に変換
 std::string to_binString(unsigned int val)
 {
