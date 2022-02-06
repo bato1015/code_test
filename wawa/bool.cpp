7e0d5d133283b57afb4aaf72b0718c7e7244bc48
Bato Riko <s20c1101jd@s.chib>
2022-02-06 22:50:58 +0900

    test

---
+++ wawa/bool.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    bool a;
+    a(false);
+    return 0;
+}
---
+++ wawa/touka.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int a = 2;
+    int b = 2;
+    int ww = (a != b);
+    std::cout << ww;
+    return 0;
+}
